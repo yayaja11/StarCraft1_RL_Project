3c8f209aa1cc3e0e83fee57374df24f3033686b4
yayaja11 <yayaja11@naver.com>
2022-02-13 13:23:12 +0900

    feat: prototype of starcraft1-gym env

---
+++ envs/single_battle_env.py
@@ -0,0 +1,126 @@
+"""
+여기서 어떤 방식의 스타크래프트 RL을 만들지 정의하는 것 같음
+"""
+
+import numpy as np
+from gym import spaces
+import TorchCraft.starcraft_gym.proto as proto
+import TorchCraft.starcraft_gym.gym_utils as utils
+
+import torchcraft.Constants as tcc
+import TorchCraft.starcraft_gym.envs.starcraft_env as sc
+
+DISTANCE_FACTOR = 16
+
+class SingleBattleEnv(sc.StarCraftEnv):
+    def __init__(self, server_ip, server_port, speed=0, frame_skip=0, self_play = False, max_episode_steps = 2000):
+        super(SingleBattleEnv, self).__init__(server_ip, server_port, speed, frame_skip, self_play, max_episode_steps)
+
+    def _action_space(self):
+        action_low = [-1.0, -1.0, -1.0]
+        action_high = [1.0, 1.0, 1.0]
+        return spaces.Box(np.array(action_low), np.array(action_high))
+
+    def _observation_space(self):
+        obs_low  = [  0.0,   0.0, 0.0, 0.0, -1.0,  0.0,  0.0,   0.0,  0.0, 0.0]
+        obs_high = [100.0, 100.0, 1.0, 1.0,  1.0, 50.0, 100.0, 100.0, 1.0, 1.0]
+        return spaces.Box(np.array(obs_low), np.array(obs_high))
+
+    def _make_commands(self, action):
+        cmds = []
+        if self.state is None or action is None:
+            return cmds
+
+        myself_id = None
+        myself = None
+        enemy_id = None
+        enemy = None
+        if self.state.units == {}:  # 아무 유닛도 아직 없을때 처리
+            return cmds
+
+        for a in self.state.units[0]:
+            myself_id = a.id
+            myself = a
+        for b in self.state.units[1]:
+            enemy_id = b.id
+            enemy = b
+
+        if action[0] > 0:
+            if myself is None or enemy is None:
+                return cmds
+            cmds.append([
+                tcc.command_unit_protected, myself_id,
+                tcc.unitcommandtypes.Attack_Unit, enemy_id])
+        else:
+            if myself is None or enemy is None:
+                return cmds
+            degree = action[1] * 180  # 180의 의미??
+            distance = (action[2] + 1) * DISTANCE_FACTOR # DISTANCE_FACTOR의 의미?
+            x2, y2 = utils.get_position(degree, distance, myself.x, -myself.y)  # myself.x -> myself.position[1]
+            cmds.append([
+                tcc.command_unit_protected, myself_id,
+                tcc.unitcommandtypes.Move, -1, int(x2), int(y2)])  # numpy.float64형식을 받을수없다고해서 int로 바꿈
+
+        return cmds
+
+    def _make_observation(self):
+        myself = None
+        enemy = None
+
+        obs = np.zeros(self.observation_space.shape)
+
+        if self.state.units == {}:  # 아무 유닛도 아직 없을때 처리
+            obs[9] = 1.0
+            return obs
+        myunits = self.state.units[0]
+        enemy_units = self.state.units[1]
+
+        for a in myunits:
+            myself = a
+
+#        for uid, ut in self.state.units[0]:  # 원래 units_myself.items
+#            myself = ut
+        for b in enemy_units:  # 원래 units_enemy.items():
+            enemy = b
+
+
+        if myself is not None and enemy is not None:  # 현재 상태??
+            obs[0] = myself.health
+            obs[1] = myself.groundCD
+            obs[2] = myself.groundRange / DISTANCE_FACTOR -1
+            obs[3] = 0.0
+            obs[4] = utils.get_degree(myself.x, -myself.y, enemy.x, -enemy.y) / 180
+            obs[5] = utils.get_distance(myself.x, -myself.y, enemy.x, -enemy.y) / DISTANCE_FACTOR - 1
+            obs[6] = enemy.health
+            obs[7] = enemy.groundCD
+            obs[8] = enemy.groundRange / DISTANCE_FACTOR -1
+            obs[9] = 1.0
+        else:
+            obs[9] = 1.0
+
+        return obs
+
+    def _check_done(self):
+        return bool(self.state.game_ended) or self.state.battle_just_ended  # state에 값을 넣는게 torchcraft에서도 맞는지
+
+    def _compute_reward(self):  #보상 계산
+        reward = 0
+        if self.obs[5] + 1 > 1.5:
+            reward = -1
+
+        if self.obs_pre[6] > self.obs[6]:
+            reward = 15
+        if self.obs_pre[0] > self.obs[0]:
+            reward = -10
+        if self._check_done() and not bool(self.state.battle_won):
+            reward = -500
+        if self._check_done() and bool(self.state.battle_won):
+            reward = 1000
+            self.episode_wins += 1
+        if self.episode_steps == self.max_episode_steps:
+            reward = -500
+
+        return reward
+
+    def _get_info(self):
+        return {}
---
+++ envs/starcraft_env.py
@@ -0,0 +1,107 @@
+import gym
+import torchcraft as tc
+import torchcraft.Constants as tcc
+
+import TorchCraft.starcraft_gym.proto as proto
+import TorchCraft.starcraft_gym.gym_utils as utils
+
+
+class StarCraftEnv(gym.Env):
+    def __init__(self, server_ip, server_port, speed, frame_skip, self_play, max_episode_steps):
+        self.client = tc.Client()  # torchcrafy_py에선 여기에 ip랑 port 전달
+        self.server_ip = server_ip
+        self.server_port = int(server_port)
+        self.client.connect(self.server_ip, self.server_port)
+        self.state = self.client.init(micro_battles=True)   #  setup state
+        print(self.state.player_info)
+        self.speed = speed
+        self.frame_skip = frame_skip
+        self.self_play = self_play
+        self.max_episode_steps = max_episode_steps
+
+        self.episodes = 0
+        self.episode_wins = 0
+        self.episode_steps = 0
+
+        self.action_space = self._action_space()
+        self.observation_space = self._observation_space()
+
+        self.state = None
+        self.obs = None
+        self.obs_pre = None
+        print('6')
+    def __del__(self):
+        self.client.close()
+
+    def _step(self, action):
+        self.episode_steps += 1
+
+        self.client.send(self._make_commands(action))
+        self.state = self.client.recv()   # torchcraft_py에선 receive
+        self.obs = self._make_observation()
+        reward = self._compute_reward()
+        done = self._check_done()
+        info = self._get_info()
+
+        self.obs_pre = self.obs
+        print(self.episode_steps,':',self.obs, reward, done, info)
+        return self.obs, reward, done, info
+
+    def _reset(self):
+        print('7')
+
+        utils.print_progress(self.episodes, self.episode_wins)
+#        print(not self.self_play, self.episode_steps == self.max_episode_steps)
+        if not self.self_play and self.episode_steps == self.max_episode_steps:  # 종료
+            print('step reach max')
+            print([tcc.restart])
+            self.client.send([tcc.restart])
+            self.state = self.client.recv()
+            while not bool(self.client.state.game_ended):
+                self.client.send([])
+                self.state = self.client.recv()
+
+        self.episodes += 1
+        self.episode_steps = 0
+
+        print(self.episode_steps)
+        # self.client.send([tcc.restart])
+
+        self.client.close()
+        self.client.connect(self.server_ip, self.server_port)
+        self.state = self.client.init(micro_battles=True)
+
+        setup = [[tcc.set_speed, self.speed],  # proto concat_Cmd 다 빼고 list로 만들었음
+                 [tcc.set_gui, 1],
+                 [tcc.set_frameskip, self.frame_skip],
+                 [tcc.set_cmd_optim, 1]]
+
+        self.client.send(setup)
+        self.state = self.client.recv() # self.state = self.client.state.d 라는 torchcraft py를 변환한 버전
+
+        self.obs = self._make_observation()
+        self.obs_pre = self.obs
+        return self.obs
+
+
+
+        def _action_space(self):
+            raise NotImplementedError
+
+        def _observation_space(self):
+            raise NotImplementedError
+
+        def _make_commands(self, action):
+            raise NotImplementedError
+
+        def _make_observation(self):
+            raise NotImplementedError
+
+        def _compute_reward(self):
+            raise NotImplementedError
+
+        def _check_done(self):
+            return bool(self.state.game_ended) or self.state.battle_just_ended  # state에 값을 넣는게 torchcraft에서도 맞는지
+
+        def _get_info(self):
+            return {}
