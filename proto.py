e91519bfef32b9ae7c60793e3503331ddec436d4
yayaja11 <yayaja11@naver.com>
2022-03-21 12:46:12 +0900

    change

--- proto.py
+++
@@ -1,7 +0,0 @@
-def concat_cmd(cmd, *args):
-    result = cmd
-    for arg in args:
-       result = str(result) + ',' + str(arg)
-#    for arg in args:
-#        result.append(str(arg))
-    return result
